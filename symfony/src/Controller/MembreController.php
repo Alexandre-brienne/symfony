9dcbf98c7817f764300825e31ad6d6a8c560b6b6
alexgrapher <briennealexandre@gmail.com>
2021-02-17 12:58:06 +0100

    ajout code image

--- symfony/src/Controller/MembreController.php
+++ symfony/src/Controller/MembreController.php
@@ -8,21 +8,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 #[Route('/membre')]
 class MembreController extends AbstractController
 {
-    #[Route('/', name: 'membre_index',methods: ['GET'])]
-    public function index(AnnonceRepository $annonceRepository): Response
-    {
-        return $this->render('membre/index.html.twig', [
-            'controller_name' => 'MembreController',
-            'annonces' => $annonceRepository->findAll(),
-        ]);
-    }
+
 
     #[Route('/create', name: 'membre_new', methods: ['GET', 'POST'])]
-    public function new(Request $request): Response
+    public function new(Request $request, SluggerInterface $slugger): Response
     {
         $annonce = new Annonce();
         $form = $this->createForm(AnnonceType::class, $annonce);
@@ -33,6 +27,37 @@ class MembreController extends AbstractController
             $annonce->setDatePublication(new \DateTime());
             $userconnecte = $this->getUser();
             $annonce->setUser($userconnecte);
+
+            
+            // code de gestion de upload image
+            $imageFile = $form->get('image')->getData();
+
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'imageFilename' property to store the PDF file name
+                // instead of its contents
+                $annonce->setImage($newFilename);
+            }else {
+                $annonce->setImage('');
+            }
+
+
 
             
             $entityManager = $this->getDoctrine()->getManager();
@@ -49,4 +74,23 @@ class MembreController extends AbstractController
         ]);
     }
     
+    #[Route('/', name: 'membre_index',methods: ['GET'])]
+    public function index(AnnonceRepository $annonceRepository): Response
+    {
+        $userConnecte = $this->getUser();
+        return $this->render('membre/index.html.twig', [
+            'controller_name' => 'MembreController',
+            'annonces' => $annonceRepository->findby([
+                "user" => $userConnecte 
+            ]),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'membre_show', methods: ['GET'])]
+    public function show(Annonce $annonce): Response
+    {
+        return $this->render('annonce/show.html.twig', [
+            'annonce' => $annonce,
+        ]);
+    }
 }
--- symfony/src/Form/AnnonceType.php
+++ symfony/src/Form/AnnonceType.php
@@ -6,7 +6,8 @@ use App\Entity\Annonce;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 class AnnonceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -14,7 +15,28 @@ class AnnonceType extends AbstractType
         $builder
             ->add('titre')
             ->add('contenu')
-            ->add('image')
+            ->add('image',FileType::class, [
+                'label' => 'choisissez une photo à uploader',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                         
+                        ],
+                        'mimeTypesMessage' => 'type de fichier incorrect',
+                    ])
+                ],
+            ])
             ->add('slug')
             // ->add('datePublication')
         ;
